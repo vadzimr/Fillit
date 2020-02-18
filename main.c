a6a84e3e27e1a05ffe4cdba1226b95cbe9cf694d
Emily wing lum Cheung <echeung@e1z2r7p1.42.us.org>
2020-02-18 15:19:58 -0800

    main

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: echeung <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/09 18:27:38 by echeung           #+#    #+#             */
-/*   Updated: 2020/02/17 18:05:36 by echeung          ###   ########.fr       */
+/*   Updated: 2020/02/18 15:19:33 by echeung          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	main(int ac, char **av)
 {
 	if (ac != 2)
 	{
-		ft_putstr("usage: ./fillit input_file\n");
+		ft_putstr("usage: ./fillit source_file\n");
 		return (1);
 	}
 	if (read_file(av[1]) == 0)
